1cc779d9a07ec08e4da1e7fb3910d8ba3fc9a897
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2022-10-31 18:14:44 -0700

    deploying to Heroku

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 // server/index.js
-
 const express = require("express")
+const path = require("path")
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
+
+// Node serves the static files for built app
+app.use(express.static(path.resolve(__dirname, "../client/build")))
+
+// Handle GET requests for /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" })
+})
+
+//All other GET requests not handle before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`)
